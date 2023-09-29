67d56b8e1242424e369021cc8a762c3481f9e0bc
Josue vasquez <avasquezl13@miumg.edu.gt>
2023-09-29 15:38:36 -0600

    agregando librerias

--- src/app/modules/public/inicio/inicio.component.ts
+++ src/app/modules/public/inicio/inicio.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit{
+
+  public url ='https://github.com/Josue19688';
+
+  private router = inject(Router);
+
+
+
+  ngOnInit(): void {
+    const options = {
+      strings: [
+        'Hola!! mi nombre es, <br>Josue Vasquez',
+        'Soy desarrollador FullStack'],
+      typeSpeed: 100,
+      backSpeed: 20,
+      showCursor: true,
+      cursorChar: '',
+      loop: true
+    };
+    const typed = new Typed('.typed-user', options);
+  }
+
+
+ 
 
 }
